57acc5925bba669af848a5f48892a3f80f5f980d
Mihir Karandikar <mihirkarandikar@scs-ma95.harbinger.in>
2016-03-09 13:54:30 +0530

    testing connectivity

---
+++ app.js
@@ -0,0 +1,11 @@
+var element = document.querySelector('#click');
+
+element.addEventListener('click', function(){
+	return navigator.bluetooth.requestDevice({ fliters: [{services:['battery_service']}] })
+	.then(device => { device.connectGATT(); })
+	.then(server => { return server.getPrimaryService('battery_service'); })
+	.then(service => { return service.getCharacteristic('battery_level'); })
+	.then(characteristic => { return characteristic.readValue(); })
+	.then(value => { value = value.buffer ? value : new DataView(value); console.log("Battery level: " + value.getUint(0)); })
+	.catch(error => { console.log(error); });
+});
