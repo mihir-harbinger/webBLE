c691d5c2b8c019969b4183df3daa7164daf0672e
Mihir Karandikar <mihirkarandikar@scs-ma95.harbinger.in>
2016-03-09 14:04:51 +0530

    testing connectivity

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var elem = document.querySelector('#click');
 
 elem.addEventListener('click', function(){
 	console.log('clicked!');
-	return navigator.bluetooth.requestDevice({ filters: [{services:['battery_service']}] })
+	navigator.bluetooth.requestDevice({ filters: [{ services: ['battery_service'] }] })
 	.then(device => { device.connectGATT(); })
 	.then(server => { return server.getPrimaryService('battery_service'); })
 	.then(service => { return service.getCharacteristic('battery_level'); })
